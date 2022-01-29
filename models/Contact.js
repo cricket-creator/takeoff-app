be17b090b57db7347ec21d87dc23cbab0c26a37d
Pavel Rakhaev <rakhaevpavel@gmail.com>
2022-01-29 14:37:59 +0300

    Added React

--- models/Contact.js
+++ models/Contact.js
@@ -1,7 +1,7 @@
 const { Schema, model, Types } = require('mongoose');
 
 const contactSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, minLength: 1 },
   phone: { type: String, required: true, unique: true, minLength: 10, maxLength: 15 },
   owner: { type: Types.ObjectId, ref: 'User' },
 });
